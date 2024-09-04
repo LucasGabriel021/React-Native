a9f3fb6b3e2caff17253ee8b5f9db05242bbd0a8
Lucas Gabriel <lucasgabrielgomes33@gmail.com>
2024-09-03 23:00:16 -0300

    Projeto Concluido

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, View } from 'react-native';
 import Cesta from "./src/views/Cesta";
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from "@expo-google-fonts/montserrat";
+import mock from "./src/mocks/cesta";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from 'react';
 
 export default function App() {
   const [fontCarregada] = useFonts({
@@ -9,13 +12,31 @@ export default function App() {
     "MontserratBold": Montserrat_700Bold,
   });
 
-  if(!fontCarregada) {
-    return <View/>
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync(); // A tela permanecerá visível até que o hideAsync seja chamado.
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  useEffect(() => {
+    if(fontCarregada) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontCarregada]);
+
+  if (!fontCarregada) {
+    return <View />;
   }
   
   return (
-    <SafeAreaView>
-      <Cesta/>
+    <SafeAreaView style={{flex: 1}}>
+      <Cesta {...mock}/>
     </SafeAreaView>
   );
 }
--- src/views/Cesta/components/Detalhes.js
+++ src/views/Cesta/components/Detalhes.js
@@ -1,17 +1,18 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Texto from "../../../components/Texto";
-import logo from "../../../../assets/logo.png"
+import Botao from "../../../components/Botao";
 
-export default function Detalhes() {
+export default function Detalhes({ nome, logoFazenda, nomeFazenda, descricao, preco, botao }) {
      return <>
-          <Texto style={estilos.nome}>Cesta de Verduras</Texto>
+          <Texto style={estilos.nome}>{nome}</Texto>
           <View style={estilos.fazenda}>
-               <Image source={logo} style={estilos.imagemFazenda}/>
-               <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
+               <Image source={logoFazenda} style={estilos.imagemFazenda}/>
+               <Texto style={estilos.nomeFazenda}>{nomeFazenda}</Texto>
           </View>
-          <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
-          <Texto style={estilos.preco}>R$ 40,00</Texto>
+          <Texto style={estilos.descricao}>{descricao}</Texto>
+          <Texto style={estilos.preco}>{preco}</Texto>
+          <Botao texto={botao} style={estilos.botao} onPress={() => {}}></Botao>
      </>
 }
 
@@ -46,5 +47,8 @@ const estilos = StyleSheet.create({
           fontSize: 26,
           lineHeight: 42,
           marginTop: 8,
-     }
+     },
+     botao: {
+          marginTop: 16,
+     },
 });
